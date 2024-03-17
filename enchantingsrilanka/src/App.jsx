7bce9d5daf972edc728fd94896309161c25df61e
hash-2002 <136227358+hash-2002@users.noreply.github.com>
2024-03-18 01:56:33 +0530

    project initialized
    signup and login pages created

---
+++ enchantingsrilanka/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
+function App() {
+  return (
+    <div>
+      <SignUp />
+      {/* <Login /> */}
+    </div>
+  );
+}
+
+export default App;
